81a85dee78c43d8a2fe12d0823ef0fa98732250a
Guilherme Mendes <guilheu2004@gmail.com>
2024-11-09 22:30:19 -0300

    adicao dos arquivos
    adicionei finalmente mds do ceu

---
+++ teste/resources/js/components/Products.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import {ref} from 'vue';
+    const name = ref('');
+</script>
+<style>
+    h2{
+        word-wrap: break-word;
+    }
+</style>
+<template>
+    <h2> {{name}}</h2>
+    
+    <br>
+    <input type="text" v-model="name">
+</template>
